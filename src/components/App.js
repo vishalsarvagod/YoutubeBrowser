94d18eeb2871ef0d7e46fa3253c390fbc98b26e7
Vishal Sarvagod <vishalsarvagod91@gmail.com>
2021-02-16 11:22:08 -0500

    youtubeBrowser

---
+++ src/components/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import SearchBar from './SearchBar';
+import youtube from '../api/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
+
+class App extends React.Component {
+    state = { videos: [], selectedVideo: null};
+    componentDidMount(){
+        this.onTermSubmit('buildings');
+    }
+
+    onTermSubmit =async term =>{
+       const response = await youtube.get('/search',{
+            params:{
+                q: term
+            }
+        });
+        this.setState({videos: response.data.items, selectedVideo: response.data.items[0]});
+    };
+    onVideoSelect =(video) => {
+        this.setState({selectedVideo: video});
+        //console.log('from the app!', video);
+    };
+    render(){
+        return (
+            <div className="ui container">
+                <SearchBar onFormSubmit={this.onTermSubmit}></SearchBar>
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo}></VideoDetail>
+                        </div>
+                        
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}></VideoList>
+                        </div>
+                    </div>
+                </div>
+               
+            </div>
+        );
+    }
+}
+
+export default App;
